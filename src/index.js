5e5d363fa11f75a9f1b2d89753d3d742630c66d3
hieusmiths <hieusmiths@gmail.com>
2019-12-04 00:54:28 +0700

    config loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const _  = require('lodash');
+import './style.css'
+import Cat from './cat.jpg'
 
   function component() {
     const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+    element.classList.add('hello');
+    const MyCat  = new Image()
+    MyCat.src = Cat
+    element.appendChild(MyCat)
     return element;
   }
 
